5a2668afc6b7fb7644b2c3fdfa146c5f9e5b6b19
wisdom-plus <tomoron326@outlook.jp>
2025-12-21 08:01:31 +0900

    fix:type

--- cmd/version.go
+++ cmd/version.go
@@ -18,7 +18,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the version of Dottan CLI",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Dottan CLI %s", version)
+		fmt.Println("Dottan CLI", version)
 	},
 }
 
