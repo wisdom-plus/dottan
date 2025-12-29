46d274548c2f0fbef599f5521dbea52009cf21f6
wisdom-plus <tomoron326@outlook.jp>
2025-12-29 18:52:51 +0900

    add:config init command

---
+++ cmd/config_init.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"/dottan/internal/config"
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var configInitCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize Dottan config file",
+	Long:  `Initialize Dottan config file in the user config directory.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		const appName = "dottan"
+		path, err := config.ConfigPath(appName)
+		if err != nil {
+			return err
+		}
+
+		if config.Exists(path) && !force {
+			return fmt.Errorf("%w: %s (use --force to overwrite)", config.ErrorAlreadyExists, path)
+		}
+		cfg := &config.Config{
+			DefaultProfile: "personal",
+			Profiles: map[string]config.Profile{
+				"personal": {
+					GitHubURL: "https://github.com"
+				}
+			}
+		}
+		if err := config.Save(path, cfg); err != nil {
+			return err
+		}
+
+		fmt.Fprintf(cmd.OutOrStdout(), "created: %s\n", path)
+		return nil
+	},
+}
+
+func init() {
+	configCmd.AddCommand(configInitCmd)
+	configInitCmd.Flags().BoolVar(&force, "force", false, "overwrite existing config file")
+}
